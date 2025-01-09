28516d44fb88ab2bede611b3d5022989f9a23db6
KOSASIH <kosasihg88@gmail.com>
2025-01-09 23:38:13 +0700

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,16 @@
+# utils/logger.py
+
+import logging
+import sys
+
+def setup_logging(level=logging.INFO):
+    """Set up logging configuration."""
+    logging.basicConfig(
+        level=level,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.StreamHandler(sys.stdout),  # Log to standard output
+            logging.FileHandler("door_for_pi.log")  # Log to a file
+        ]
+    )
+    logging.info("Logging is set up.")
